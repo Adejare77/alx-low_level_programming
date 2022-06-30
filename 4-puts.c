75ce22fbe19ea8ab83b5cfaf339f350ba99b1fb4
Rashisky <root@Rashisky-Paper.localdomain>
2022-06-30 20:45:06 +0100

    Hello, World

---
+++ 4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Program to test Code
+ *
+ * Returns: 0 means Success, else 1
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
