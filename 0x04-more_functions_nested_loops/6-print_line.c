5d19ff7c15fbab716713532b2cf2eb62b25ecbd9
Rashisky <root@Rashisky-Paper.localdomain>
2022-07-07 22:16:06 +0100

    More functions and nested loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -13,10 +13,13 @@ void print_line(int n)
 
 	if (n <= 0)
 		_putchar('\n');
-	while (i < n)
+	else
 	{
-		_putchar('_');
-		i++;
+		while (i < n)
+		{
+			_putchar('_');
+			i++;
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
