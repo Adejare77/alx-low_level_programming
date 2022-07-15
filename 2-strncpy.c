50cfdc39bc1ae342806194ef2dff36b64d6a510a
Adejare <adejare.arasheed@gmail.com>
2022-07-15 03:25:50 +0100

    More pointers, arrays and strings

---
+++ 2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - function that copies a string
+ * @dest: first string
+ * @src: second string
+ * @n: number of character to be copied from the second string
+ * Return: pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while ((src[i] != '\0') && (i < n))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	if (src[i] == '\0')
+		dest[i] = '\0';
+
+	return (dest);
+}
--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 #!/bin/bash
-This is my first introduction to C programming. Sponsored
-by ALX Africa Scholarhip. My profound thanks to Holberton School
-for their unwavering dedication towards teaching us how to to be a good programmer, both in coding and following the standard writing code. Here we go betty
+More pointers, arrays and strings
