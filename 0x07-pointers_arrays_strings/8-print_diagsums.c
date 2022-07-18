beb0c0b4b205f3c64232c8aff566302f1c31e75e
Adejare <adejare.arasheed@gmail.com>
2022-07-18 20:59:10 +0100

    Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_diagsums - function that prints the sum of two
@@ -11,14 +12,17 @@
 void print_diagsums(int *a, int size)
 {
 	int i = 0, n = size - 1;
-	int sum_1 = 0, sum_2 = size, res = 0;
+	int sum_1 = 0, sum_2 = 0;
+	int n_size = size*size;
 
-	while (i < (size * *2))
+	while (i < n_size)
 	{
 		sum_1 += a[i];
 		i += size + 1;
 
-		sum_2 = a[n];
+		sum_2 += a[n];
 		n += size - 1;
 	}
+
+	printf("%d, %d\n", sum_1, sum_2);
 }
