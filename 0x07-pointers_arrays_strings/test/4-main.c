913f07428c9048a1087d89fb8e2b4f49c7a9c60d
Adejare <adejare.arasheed@gmail.com>
2022-07-25 19:32:07 +0100

    More pointers, arrays and strings

--- 0x07-pointers_arrays_strings/test/4-main.c
+++ 0x07-pointers_arrays_strings/test/4-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code
@@ -12,7 +11,7 @@ int main(void)
 	char *f = "world";
 	char *t;
 
-	t = _strstr(s, f);
+	t = _strpbrk(s, f);
 	printf("%s\n", t);
 	return (0);
 }
