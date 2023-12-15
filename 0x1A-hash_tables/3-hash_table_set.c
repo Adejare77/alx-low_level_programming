f16765e334860de4c2b834ab11821ca3c5219b1c
Adejare <adejare.arasheed@gmail.com>
2023-12-15 20:47:49 +0100

    Hash Data Structure

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -40,9 +40,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				else
 					prev_val_hldr->next = element;
 				check = 0;
-				free(tmp->key);
-				free(tmp->value);
-				free(tmp);
 			}
 			prev_val_hldr = tmp;
 			tmp = tmp->next;
