d14adecc59799e5771ec3a2a00b6ed97563eaa03
Adejare <adejare.arasheed@gmail.com>
2023-12-15 20:38:22 +0100

    Hash Data Structure

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -79,9 +79,12 @@ unsigned long int key_idx)
 			else
 				prev_val_hldr->next = element;
 			check = 0;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
 		}
 		prev_val_hldr = tmp;
-		tmp = tmp ? tmp->next : NULL;
+		tmp = tmp->next;
 	}
 
 	return (check);
