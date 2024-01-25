e7f5cd64179faeb86ae4580631b42a5a34c48d8a
Adejare <adejare.arasheed@gmail.com>
2024-01-25 02:50:46 +0100

    Island_perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,13 +9,12 @@ def island_perimeter(grid):
     island = 0
     for v in range(len(grid)):
         for h in range(len(grid[v])):
-            if (v != 0 and grid[v - 1][h] == 1):
-                overlapped_perimeter += 1
-            if (h != 0 and grid[v][h - 1] == 1):
-                overlapped_perimeter += 1
+            if (v != 0 and grid[v - 1][h] == 1 and grid[v][h] == 1):
+                overlapped_perimeter += 2
+            if (h != 0 and grid[v][h - 1] == 1 and grid[v][h] == 1):
+                overlapped_perimeter += 2
             if (grid[v][h] == 1):
                 island += 1
     total_island = 4 * island  # since the island is square
-    total_overlapped_perimeter = 2 * overlapped_perimeter
-    perimeter = total_island - total_overlapped_perimeter
+    perimeter = total_island - overlapped_perimeter
     return perimeter
