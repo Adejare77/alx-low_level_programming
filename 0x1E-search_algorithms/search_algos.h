fe2b15ea98ed30f91b256a8b0400b28f3f3a7307
Adejare <adejare.arasheed@gmail.com>
2024-03-12 14:50:16 +0100

    Search Algo header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,16 @@
+#ifndef _search_algorithms_
+#define _search_algorithms_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int bin_search_algo(int *array, size_t low, size_t high, int value);
+int binary_search(int *array, size_t size, int value);
+int binary_search(int [], size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int interpolation_algo(int *array, size_t low, size_t high, int value);
+
+
+#endif
