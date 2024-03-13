5c8adeeda85e1217e96d7ae2fc3846713a4739b3
Adejare <adejare.arasheed@gmail.com>
2024-03-13 14:54:17 +0100

    Search Algorithm Header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,6 +4,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
+
 int linear_search(int *array, size_t size, int value);
 int bin_search_algo(int *array, size_t low, size_t high, int value);
 int binary_search(int *array, size_t size, int value);
@@ -15,6 +32,6 @@ int exponential_search(int *array, size_t size, int value);
 int exp_bin_search_algo(int *array, size_t low, size_t high, int value);
 int advanced_binary(int *array, size_t size, int value);
 int adv_bin_search_algo(int *array, size_t low, size_t high, int value);
-
+int jump_list(listint_t *list, size_t size, int value);
 
 #endif
