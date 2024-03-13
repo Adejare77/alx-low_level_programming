1b531059faade64c0a154331f8a1f08a42fc793b
Adejare <adejare.arasheed@gmail.com>
2024-03-13 12:50:17 +0100

    Search Algorithm Header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,6 +11,10 @@ int binary_search(int [], size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int interpolation_algo(int *array, size_t low, size_t high, int value);
+int exponential_search(int *array, size_t size, int value);
+int exp_bin_search_algo(int *array, size_t low, size_t high, int value);
+int advanced_binary(int *array, size_t size, int value);
+int adv_bin_search_algo(int *array, size_t low, size_t high, int value);
 
 
 #endif
