e027b47a2d6f79463efa7d3b43c2a3bec372aa13
Adejare <adejare.arasheed@gmail.com>
2024-03-13 14:54:35 +0100

    Backup

--- 0x1E-search_algorithms/backup/104-advanced_binary.c
+++ 0x1E-search_algorithms/backup/104-advanced_binary.c
@@ -17,6 +17,7 @@ int advanced_binary(int *array, size_t size, int value)
 	if (!array)
 		return (-1);
 
+	/* using size instead of index */
 	index = adv_bin_search_algo(array, 0, size, value);
 	return (index);
 }
@@ -33,6 +34,7 @@ int advanced_binary(int *array, size_t size, int value)
 */
 int adv_bin_search_algo(int *array, size_t low, size_t high, int value)
 {
+	/* Effect of using size instead of index */
 	size_t mid = (low + high) % 2 == 0 ? (low + high) / 2 : ((
 		low + high) / 2) + 1;
 	size_t index = low;
